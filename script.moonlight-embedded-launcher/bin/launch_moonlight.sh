ce537427d6affce9cee3c4b330c96b6e82681bee
clarkemw <mcs5280@gmail.com>
2021-05-07 21:14:22 -0600

    Fix quitflag in launcher script

--- script.moonlight-embedded-launcher/bin/launch_moonlight.sh
+++ script.moonlight-embedded-launcher/bin/launch_moonlight.sh
@@ -37,7 +37,7 @@ systemctl stop kodi # Must close kodi for proper video display
 # Adjusted to just used input variables 
 docker run --rm --name moonlight -t -v moonlight-home:/home/moonlight-user \
 -v /var/run/dbus:/var/run/dbus --device /dev/vchiq --device /dev/input \
-clarkemw/moonlight-embedded-raspbian stream -"$res" -fps "$fps" -bitrate "$bitrate" -app "$game" "$quitflag"
+clarkemw/moonlight-embedded-raspbian stream -"$res" -fps "$fps" -bitrate "$bitrate" $quitflag -app "$game"
 
 docker wait moonlight
 
